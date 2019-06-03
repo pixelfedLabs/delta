1720098ed84d481578e4d98ecfaba72fb0f5f65f
Daniel Supernault <danielsupernault@gmail.com>
2019-06-02 22:29:10 -0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,8 @@
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",
+                 Roboto,Helvetica,Arial,sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
