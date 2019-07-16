5ecb06dea0214f502e5cc42e306aa6f9aaa7ab15
Daniel Supernault <danielsupernault@gmail.com>
2019-07-15 20:49:14 -0600

    wip

---
+++ resources/views/account/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('account.layout')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+				<div class="card-header">Dashboard</div>
+				<div class="card-body p-5"></div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,11 @@
 <?php
 
-Route::get('/', 'SiteController@welcome');
-Route::get('join', 'SiteController@join');
+Route::domain(config('delta.domain.app'))->group(function() {
+	Route::get('/', 'AccountController@home');
+	Auth::routes();
+});
+
+Route::domain(config('delta.domain.landing'))->group(function() {
+	Route::get('/', 'SiteController@welcome');
+	Route::get('join', 'SiteController@join');
+});
